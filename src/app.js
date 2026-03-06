881f5347c14adf8dadd3d188545bf5a77db01910
Apurv <apurv2003londhe@gmail.com>
2026-03-06 13:16:04 +0530

    UPDATES

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ const allowedOrigins = [
 app.use(cookieParser());
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
