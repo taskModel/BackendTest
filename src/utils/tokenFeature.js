eaa052303a73394b8af451dfaa69cbf4d762133a
Apurv <apurv2003londhe@gmail.com>
2026-03-06 12:11:14 +0530

    cookie update

--- src/utils/tokenFeature.js
+++ src/utils/tokenFeature.js
@@ -10,8 +10,8 @@ export const sendToken = (res, status = 200, message, user) => {
     .cookie("token", token, {
       httpOnly: true,
       maxAge: 15 * 60 * 1000,
-      sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
-      secure: process.env.NODE_ENV === "Development" ? false : true,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production",
     })
     .json({
       success: true,
